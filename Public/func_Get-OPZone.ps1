4730cc8f328f3cdbfb9263d3f58b58e4335b7012
Jeshua Edgar <jeshuaedgar@tredion.nl>
2022-11-14 14:27:11 +0100

    Get zone ID for both sectigo and openprovider zones

---
+++ Public/func_Get-OPZone.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    Get the Zone ID for a domain
+.DESCRIPTION
+    Get the Zone ID for a domain, specify whether to look for OpenProvider free zones or premium Sectigo DNS zones
+.EXAMPLE
+    Get-OPZone -Domain "testdomain.com" -Sectigo
+#>
+
+
+function Get-OPZone {
+    param (
+        [string]$Domain,
+        [switch]$OpenProvider,
+        [switch]$Sectigo
+    )
+    if ($Sectigo) {
+        $provider = "sectigo"
+    }
+    if ($OpenProvider) {
+        $provider = "openprovider"
+    }
+    $request_body = @{
+        provider = $provider
+    }
+    try {
+        $ErrorActionPreference = 'Stop'
+        return (Invoke-RestMethod -method get "https://api.openprovider.eu/v1beta/dns/zones/$($Domain)" -Authentication bearer -Token $op_auth_token -Body $request_body).data.id
+    }
+    catch {
+        Write-Error "Cannot find Zone for domain $Domain"
+    }
+}
