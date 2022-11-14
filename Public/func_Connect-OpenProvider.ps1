2ad569ab862a8dd5cd2d18521b2d4c21021eb897
Jeshua Edgar <jeshuaedgar@tredion.nl>
2022-11-14 13:47:23 +0100

    Connect and disconnect

---
+++ Public/func_Connect-OpenProvider.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    Connects to the OpenProvider API
+.DESCRIPTION
+    Connects to the OpenProvider API and test whether there is a good connection
+.INPUTS
+    -Credential : OpenProvider credentials
+.EXAMPLE
+    Connect-OP
+    or:
+    Connect-OP -Credential (Get-Credential)
+#>
+
+
+function Connect-OpenProvider {
+    param(
+        [pscredential]$Credential
+    )
+    if (!$Credential) {
+        $credential = Get-Credential
+    }
+    $encrypted_data = ConvertFrom-SecureString $Credential.Password
+    $password_securestring = ConvertTo-SecureString $encrypted_data
+    $password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password_securestring))
+
+    $token_body = @{
+        username = $Credential.Username
+        password = $password
+    } | ConvertTo-Json
+    try {
+        Set-Variable "op_auth_token" -Value (ConvertTo-SecureString (Invoke-RestMethod -Method Post "https://api.openprovider.eu/v1beta/auth/login" -Body $token_body).data.token -AsPlainText -Force) -Scope Script
+        return $true
+    }
+    catch {
+        return $false
+        $_.desc
+    }
+}
---
+++ Public/func_Disconnect-OpenProvider.ps1
@@ -0,0 +1,18 @@
+<#
+.SYNOPSIS
+    Disconnects to the OpenProvider API
+.DESCRIPTION
+    Disconnects to the OpenProvider API when connected to a previous session
+.EXAMPLE
+    Disconnect-OpenProvider
+#>
+
+function Disconnect-OpenProvider {
+    if ($op_auth_token) {
+        Remove-Variable op_auth_token -Scope Script
+        return $true
+    }
+    else {
+        Write-Error "There was no active connection to OpenProvider"
+    }
+}
