29f757a0374a6dc68fd22cd0e1977c07987379f2
Jeshua Edgar <jeshuaedgar@icloud.com>
2022-12-19 17:24:45 +0100

    refactoring get-opdomain

--- Public/Domain/func_Get-OPDomain.ps1
+++ Public/Domain/func_Get-OPDomain.ps1
@@ -6,18 +6,13 @@ function Get-OPDomain {
     $limit = 500
 
     if ($Domain) {
-        $domain_name_pattern = $Domain.Split(".")[0]
         $domain_request_body = @{
-            limit               = $limit
-            domain_name_pattern = $domain_name_pattern
+            limit     = $limit
+            full_name = $Domain
         }
         try {
             $ErrorActionPreference = 'Stop'
             $domains = (Invoke-OPRequest -Method Get -Endpoint "domains" -Body $domain_request_body).data.results
-            # $domains = (Invoke-RestMethod -Method Get "https://api.openprovider.eu/v1beta/domains" -Authentication Bearer -Token $op_auth_token -Body $domain_request_body).data.results
-            if ($Domain.Split((".")[1])) {
-                $domains = $domains | Where-Object { $_.domain.extension -eq $Domain.Split((".")[1]) }
-            }
         }
         catch {
             Write-Error $_.Exception
@@ -29,7 +24,6 @@ function Get-OPDomain {
         $domains = @()
         $offset = 0
         $total_domains = (Invoke-OPRequest -Method Get -Endpoint "domains").data.total
-        # $total_domains = (Invoke-RestMethod -Method Get "https://api.openprovider.eu/v1beta/domains" -Authentication Bearer -Token $op_auth_token).data.total
         try {
             do {
                 $domain_request_body = @{
@@ -37,7 +31,6 @@ function Get-OPDomain {
                     offset = $offset
                 }
                 $domains += (Invoke-OPRequest -Method Get -Endpoint "domains" -Body $domain_request_body).data.results
-                # $domains += (Invoke-RestMethod -Method Get "https://api.openprovider.eu/v1beta/domains" -Authentication Bearer -Token $op_auth_token -Body $domain_request_body).data.results
                 $offset += 500
             } until (
                 $offset -ge $total_domains
