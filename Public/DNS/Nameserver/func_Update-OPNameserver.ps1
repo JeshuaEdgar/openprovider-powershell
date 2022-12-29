0ccec048c37b070ecec964315cf93beede93f1d1
Jeshua Edgar <jeshuaedgar@icloud.com>
2022-12-29 18:11:34 +0100

    added ipv6 parameter + output + logic

--- Public/DNS/Nameserver/func_Update-OPNameserver.ps1
+++ Public/DNS/Nameserver/func_Update-OPNameserver.ps1
@@ -1,19 +1,27 @@
 function Update-OPNameserver {
+    [CmdletBinding()]
     param(
         [parameter(Mandatory = $true)]
         [string]$Name,
 
         [parameter(Mandatory = $true)]
-        [string]$IP
+        [string]$IP,
+
+        [string]$IPv6
     )
     $request_body = @{
         ip   = $IP
         name = $Name
     }
+    if ($IPv6) {
+        $request_body.ip6 = $IPv6
+    }
+
     try {
         $request = Invoke-OPRequest -Method Put -Endpoint "dns/nameservers/$Name" -Body $request_body
         if ($request.data.success -eq $true) {
-            return $true
+            Write-Host "Succesfully updated nameserver $Name"
+            return $true | Out-Null
         }
     }
     catch {
