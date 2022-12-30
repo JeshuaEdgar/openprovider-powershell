ce3f4ae6f19ef3ec5b884cf1543e12a3fdc67a0b
Jeshua Edgar <jeshuaedgar@icloud.com>
2022-12-30 17:44:17 +0100

    new function for nameserver groups

---
+++ Public/DNS/Nameserver/func_Get-OPNameserverGroup.ps1
@@ -0,0 +1,23 @@
+function Get-OPNameserverGroup {
+    try {
+        $request = Invoke-OPRequest -Method Get -Endpoint "dns/nameservers/groups"
+        if ($request.data.total -ge 1) {
+            $return_object = @()
+            $request.data.results | ForEach-Object {
+                $return_object += [PSCustomObject]@{
+                    GroupName = $_.ns_group
+                    GroupID   = $_.id
+                    # not sure whether to return a whole list of nameservers
+                    # Nameservers = $_.name_servers
+                }
+            }
+        }
+        else {
+            Write-Warning "No nameserver group(s) found"
+        }
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+    return $return_object
+}
