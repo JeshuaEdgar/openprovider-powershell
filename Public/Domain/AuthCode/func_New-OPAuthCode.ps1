c5ab450c638936396f48c6a045005df088be1de5
Jeshua Edgar <jeshuaedgar@icloud.com>
2023-01-01 21:32:22 +0100

    request new auth code

---
+++ Public/Domain/AuthCode/func_New-OPAuthCode.ps1
@@ -0,0 +1,22 @@
+function New-OPAuthCode {
+    param (
+        [parameter(Mandatory = $true)]
+        [int]$DomainID
+    )
+    try {
+        $request = Invoke-OPRequest -Method Get -Endpoint "domains/$($DomainID)/authcode"
+        if ($request.data.success -eq $true) {
+            $return_object = [PSCustomObject]@{
+                DomainID = $DomainID
+                AuthCode = $request.data.auth_code
+            }
+        }
+        else {
+            Write-Warning "Could not get AuthCode for domain ID $DomainID"
+        }
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+    return $return_object
+}
