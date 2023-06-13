528a770f03791bdf750e27a25e829c05e34f61f6
Jeshua Edgar <jeshuaedgar@icloud.com>
2023-06-13 15:48:16 +0200

    #11 - pipeline, refactor, breaking changes

--- Public/DNS/ZoneRecord/func_Set-OPZoneRecord.ps1
+++ Public/DNS/ZoneRecord/func_Set-OPZoneRecord.ps1
@@ -1,56 +1,92 @@
 function Set-OPZoneRecord {
     [CmdletBinding()]
     param (
-        [parameter(Mandatory = $true)]
-        [string]$Domain,
+        [parameter(ValueFromPipeline, DontShow)]
+        [PSCustomObject]$InputObject,
 
-        [parameter(Mandatory = $true)]
-        [string]$ZoneID,
+        [parameter(ParameterSetName = "ManualInput", Position = 0)]
+        [ValidateScript({
+                if ($InputObject) {
+                    throw "Cannot accept both pipeline input and 'Record' parameter"
+                }
+                $true
+            })]
+        [PSCustomObject]$Record,
 
-        [parameter(Mandatory = $true)]
-        [array]$OriginalRecord,
+        [parameter(ParameterSetName = "ManualInput")]
+        [string]$Name,
+        
+        [parameter(ParameterSetName = "ManualInput")]
+        [int]$Priority,
 
-        [parameter(Mandatory = $true)]
-        [array]$NewRecord
+        [parameter(ParameterSetName = "ManualInput")]
+        [int]$TTL,
+
+        [parameter(ParameterSetName = "ManualInput")]
+        [string]$Value
 
     )
+    process {
+        if ($InputObject) {
+            if (-not ($Name -or $Priority -or $TTL -or $Value)) {
+                throw "No changes are defined!"
+            }
+            $Record = $InputObject
+        }
+        elseif (-not ($Record) -and -not ($Name -or $Priority -or $TTL -or $Value)) {
+            throw "No changes are defined!"
+        }
 
-    $request_body = @{
-        id      = $ZoneID
-        name    = $Domain
-        records = @{
-            update = @(
-                @{
-                    original_record = @{
-                        # still have to fix this, works for now. needed to strip domain away from name since OP API delivers the records with domain but only accepts without for changes.
-                        name  = ($OriginalRecord.name -replace $Domain, "").Trim(".")
-                        prio  = $OriginalRecord.prio
-                        ttl   = $OriginalRecord.ttl
-                        value = $OriginalRecord.value
-                        type  = $OriginalRecord.type
-                    }
-                    record          = @{
-                        name  = $NewRecord.name
-                        prio  = $NewRecord.prio
-                        ttl   = $NewRecord.ttl
-                        value = $NewRecord.value
-                        type  = $NewRecord.type
-                    }
+        $NewRecord = $Record | ConvertTo-Json -Depth 100 | ConvertFrom-Json
 
-                }
-            )
+        # Check and update the parameters for the new updated record
+        if ($PSBoundParameters.ContainsKey('Name')) {
+            $NewRecord.Name = $Name
+        }
+        if ($PSBoundParameters.ContainsKey('Priority')) {
+            $NewRecord.Priority = $Priority
+        }
+        if ($PSBoundParameters.ContainsKey('TTL')) {
+            $NewRecord.TTL = $TTL
+        }
+        if ($PSBoundParameters.ContainsKey('Value')) {
+            $NewRecord.Value = $Value
+        }
+
+        $request_body = @{
+            id      = $Record.ZoneID
+            name    = $Record.Domain
+            records = @{
+                update = @(
+                    @{
+                        original_record = @{
+                            name  = $Record.Name
+                            prio  = $Record.Priority
+                            ttl   = $Record.TTL
+                            value = $Record.Value
+                            type  = $Record.Type
+                        }
+                        record          = @{
+                            name  = $NewRecord.Name
+                            prio  = $NewRecord.Priority
+                            ttl   = $NewRecord.TTL
+                            value = $NewRecord.Value
+                            type  = $NewRecord.Type
+                        }
+
+                    }
+                )
+            }
+        }
+        try {
+            $request = Invoke-OPRequest -Method Put -Endpoint "dns/zones/$($Record.Domain)" -Body $request_body
+            if ($request.data.success -eq $true) {
+                Write-Host "Record has been succesfully set!"
+                return $true | Out-Null
+            }
+        }
+        catch {
+            throw $_.Exception.Message
         }
     }
-
-    try {
-        $request = Invoke-OPRequest -Method Put -Endpoint "dns/zones/$Domain" -Body $request_body
-        if ($request.data.success -eq $true) {
-            Write-Host "Record has been succesfully set!"
-            return $true | Out-Null
-        }
-    }
-    catch {
-        throw $_.Exception.Message
-    }
-    
 }
